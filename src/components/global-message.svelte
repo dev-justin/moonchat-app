d64275dd6c624965bdc2f200a8d4d379dde1b16f
Justin <dev.justinc@gmail.com>
2022-02-15 20:23:43 -0500

    Fixed image

--- src/components/global-message.svelte
+++ src/components/global-message.svelte
@@ -7,7 +7,11 @@
 {#each $globalChat as { message, alias, created_at }}
 	<div class="flex items-center mt-4 py-2 border-b-white border-b-2 border-opacity-20">
 		<span class="mr-4 text-accent">~</span>
-		<img class="w-10 h-10 p-1 mr-2" src="../static/images/alien-ava.png" alt="Alien" />
+		<img
+			class="w-10 h-10 p-1 mr-2"
+			src="https://img.icons8.com/external-bearicons-gradient-bearicons/64/000000/external-alien-halloween-bearicons-gradient-bearicons.png"
+			alt="Alien"
+		/>
 		<div class="flex flex-col justify-center pl-2">
 			<div>
 				<span class="text-sm text-white font-semibold">{alias}</span>
